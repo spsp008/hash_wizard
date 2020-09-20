6b4122126241a823287a66cfc5a656d3d8aa47b5
Shiv <spsparmar1729@gmail.com>
2020-09-21 02:29:14 +0530

    Added gem methods

--- hash_wizard.gemspec
+++ hash_wizard.gemspec
@@ -10,22 +10,9 @@ Gem::Specification.new do |spec|
   spec.email         = ["spsparmar1729@gmail.com"]
 
   spec.summary       = %q{This gem is useful in playing with ruby nested hashes, also in json format.}
-  spec.description   = %q{This gem has methods to delete keys from a nested hash even from arrays ans to set value of a specific key in entire hash }
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.description   = %q{This gem has methods to delete keys from a nested hash even from arrays, and to set value of a specific key in entire hash and to rename a key in the entire hash. }
+  spec.homepage      = "https://spsp008@bitbucket.org/spsp008/hash_wizard.git"
   spec.license       = "MIT"
-
-  # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
-  # to allow pushing to a single host or delete this section to allow pushing to any host.
-  if spec.respond_to?(:metadata)
-    spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
-
-    spec.metadata["homepage_uri"] = spec.homepage
-    spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-    spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
-  else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
-  end
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
--- lib/hash_wizard.rb
+++ lib/hash_wizard.rb
@@ -1,6 +1,53 @@
 require "hash_wizard/version"
 
 module HashWizard
+
+  # method to exclude any key in entire nested hash including Array
+  def self.delete_keys(hash_data, keys)
+    case hash_data
+    when Hash then hash_data = hash_data.each_with_object({}) { |(k, v), m| m[k] = delete_keys(v, keys) unless keys.include?(k); m }
+    when Array then hash_data.map! { |e| delete_keys(e, keys) }
+    end
+    hash_data
+  end
+
+  # method to modify key with another in entire nested hash including Array
+  # k_t_r is key to replac and k_t_w is key to replace
+  def self.rename_key(hash_data, k_t_r, k_w_r)
+    case hash_data
+    when Hash then hash_data = hash_data.inject({}) { |m, (k, v)| rename_key_block_code(m, k, v, k_t_r, k_w_r) }
+    when Array then hash_data.map! { |e| rename_key(e, k_t_r, k_w_r) }
+    end
+    hash_data
+  end
+
+  def self.rename_key_block_code(m, k, v, k_t_r, k_w_r)
+    if k == k_t_r
+      m[k_w_r] = rename_key(v, k_t_r, k_w_r)
+    else
+      m[k] = rename_key(v, k_t_r, k_w_r)
+    end
+    m
+  end
+
+  # method to set a particular value of a particuler key with another in entire nested hash including Array
+  def self.set_key_value(hash_data, key, value)
+    case hash_data
+    when Hash then hash_data = hash_data.inject({}) { |m, (k, v)| set_key_value_block_code(m, k, v, key, value) }
+    when Array then hash_data.map! { |e| set_key_value(e, key, value) }
+    end
+    hash_data
+  end
+
+  def self.set_key_value_block_code(m, k, v, key, value)
+    m[k] = if k == key && m[k].nil?
+      value
+    else
+      set_key_value(v, key, value)
+    end
+    m
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
